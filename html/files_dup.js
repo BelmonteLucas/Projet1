33154392d4afc0a7046c710a9bbe8d637e469ee3
BelmonteLucas <146923606+BelmonteLucas@users.noreply.github.com>
2023-10-19 17:29:47 +0200

    Delete html directory

--- html/files_dup.js
+++
@@ -1,4 +0,0 @@
-var files_dup =
-[
-    [ "TPNote3", "dir_0f8985166f39a65645de30d2d97c814c.html", "dir_0f8985166f39a65645de30d2d97c814c" ]
-];
--- html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['tpnote3_2ecpp_0',['TPNote3.cpp',['../_t_p_note3_8cpp.html',1,'']]]
-];
